300b7fbc6a2ce4f29c36c1c3f6c83b2b4340bb6a
Hayane Silva <hayane.silva23@gmail.com>
2019-02-05 17:37:46 -0200

    design de desafios

--- desafio 2/script.js
+++ desafio 2/script.js
@@ -2,7 +2,7 @@ document.getElementById("adicionar").addEventListener("click", function(){
     let lista=document.getElementById("lista");
     let itemNovo=document.getElementById("itemNovo");
     let item=document.createElement("li");
-
+    
     item.textContent=itemNovo.value;
     lista.appendChild(item);
     itemNovo.value="";
